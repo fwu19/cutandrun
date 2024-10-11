15a341d0aa18ef7d302cb83a9a2aa71c84fa0aa3
Feinan Wu <wu.feinan@gmail.com>
2024-10-11 08:50:46 -0700

    modify check_samplesheet.r to ensure the correct column order

--- bin/check_samplesheet.r
+++ bin/check_samplesheet.r
@@ -10,10 +10,12 @@ in_csv <- args[1]
 out_csv <- args[2]
 use_control <- as.logical(args[3])
 
-ss <- read.csv(in_csv) %>% 
+ss <- read.csv(in_csv) %>%
     mutate(
         control = ifelse(use_control, control, ""),
-        is_control = ifelse(use_control, is_control, 0)
+        is_control = ifelse(use_control, is_control, false)
     )
 
-ss %>% write.table(out_csv, sep = ',', quote = F, row.names = F)
+ss %>%
+    dplyr::select(id, group, replicate, control, single_end, fastq_1, fastq_2, is_control) %>%
+    write.table(out_csv, sep = ',', quote = F, row.names = F)
