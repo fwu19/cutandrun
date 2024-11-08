601229a3f50caa72e0573665b2606c5af037a4ed
Feinan Wu <fwu@fredhutch.org>
2024-11-08 13:39:42 -0800

    differential peaks

--- bin/differential_peaks.r
+++ bin/differential_peaks.r
@@ -516,8 +516,8 @@ for (conp.bed in conp.bed.all){
     dp.list[[basename(conp.bed)]] <- wrapper_one_conp(ss, cmp, tgt, conp.bed, count.txts)
     
 }
-dp[sapply(dp,is.null)] <- NULL
-if (length(dp) == 0){
+dp.list[sapply(dp.list,is.null)] <- NULL
+if (length(dp.list) == 0){
     stop (paste("No comparison was done. Check if", args[1], "and", args[2], "do not match!"))
 }
 saveRDS(dp.list, 'dp.rds')
